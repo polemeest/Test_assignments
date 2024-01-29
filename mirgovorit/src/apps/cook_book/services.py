b14a75f264cca1f3d32150aa931f07da1dd9a0f6
polemeest <doran6112@gmail.com>
2024-01-29 20:02:05 +0400

    created logging and header, made few fixes to views

--- mirgovorit/src/apps/cook_book/services.py
+++ mirgovorit/src/apps/cook_book/services.py
@@ -1,15 +1,16 @@
-''' Модуль для бизнес-логики поварской книги. Настраивает логирование, и:
+""" Модуль для бизнес-логики поварской книги. Настраивает логирование, и:
         Содержит функции: 
             :get_recipe_model: - Принимает ID рецепта и отрабатывает возможные ошибки при получении
     экземпляра рецепта, возвращая его, если ошибок нет.
             :change_recipe: - Принимает модель рецепта, и добавляет ему продукт, если его нет, или
-    меняет его вес.'''
+    меняет его вес."""
 
 import os
 import logging
 
 from config.settings import BASE_DIR
 from datetime import datetime
+from django.http import HttpResponseNotFound
 from django.shortcuts import redirect
 from django.urls import reverse
 from typing import Union
@@ -20,11 +21,11 @@ from .models import Recipe, RecipeProduct, Product
 # настройки для логгера:
 logger = logging.getLogger(__name__)
 logger.setLevel(logging.DEBUG)
-log_file = os.path.join(BASE_DIR, 'logs', f'log_{datetime.now().strftime('%Y-%m-%d')}.txt')
+log_file = os.path.join(BASE_DIR, "logs", f"log_{datetime.now().strftime("%Y-%m-%d")}.txt")
 
 file_handler = logging.FileHandler(log_file)
 file_handler.setLevel(logging.DEBUG)
-formatter = logging.Formatter('%(asctime)s - %(levelname)s - %(message)s')
+formatter = logging.Formatter("%(asctime)s - %(levelname)s - %(message)s")
 file_handler.setFormatter(formatter)
 
 logger.addHandler(file_handler)
@@ -32,34 +33,34 @@ logger.addHandler(file_handler)
 
 # Основные функции:
 def get_recipe_model(recipe_id: int) -> Union[Recipe, redirect]:
-    ''' Принимает ID рецепта и отрабатывает возможные ошибки при получении
-    экземпляра рецепта, возвращая его, если ошибок нет. '''
+    """ Принимает ID рецепта и отрабатывает возможные ошибки при получении
+    экземпляра рецепта, возвращая его, если ошибок нет. """
     try:
         return Recipe.objects.get(id=recipe_id)
     
     except Recipe.DoesNotExist as e:                 ## здесь можно добавить информацию из request, но сейчас не будем
-        logging.error(f'запрошена несуществующая модель рецепта с id ({recipe_id})') 
-        return redirect(reverse('homepage') + f'?error_param={e}') 
+        logging.error(f"запрошена несуществующая модель рецепта с id ({recipe_id})") 
+        return redirect(reverse("homepage") + f"?error_param={e}") 
     
     except Recipe.MultipleObjectsReturned as e:
         count = 0, 
-        for index, instance in enumerate(Recipe.objects.all().order_by('-created_at')):
+        for index, instance in enumerate(Recipe.objects.all().order_by("-created_at")):
             if index == 0:
                 continue
             instance.delete()
             count += 1
-        logging.debug(f'Получены множественные копии одного и того же рецепта. Удалено {count} старых записей')
-        return redirect(reverse('homepage') + f'?error_param={e}') 
+        logging.debug(f"Получены множественные копии одного и того же рецепта. Удалено {count} старых записей")
+        return redirect(reverse("homepage") + f"?error_param={e}") 
     
 
 def change_recipe(model: Recipe, product_id: int, weight: int) -> None:
-    ''' Принимает модель рецепта, и добавляет ему продукт, если его нет, или
-    меняет его вес. '''
+    """ Принимает модель рецепта, и добавляет ему продукт, если его нет, или
+    меняет его вес. """
     try:
         product = Product.objects.get(id=product_id)
-    except (Product.DoesNotExist, Product.MultipleObjectsReturned) as e:
-        logger.error(f'У нас проблемы с продуктом под id {product_id}')
-        raise e
+    except (Product.DoesNotExist, Product.MultipleObjectsReturned):
+        logger.error(f"У нас проблемы с продуктом под id {product_id}")
+        return HttpResponseNotFound
     products = model.product.all()
     # Если продукт уже есть:
     if product in [prod for prod in products]:
@@ -74,7 +75,7 @@ def change_recipe(model: Recipe, product_id: int, weight: int) -> None:
 
 
 def raise_cooking_amount(recipe: Recipe) -> None:
-    ''' Принимает id рецепта, и увеличивает популярность продуктов, в составе на 1. '''
+    """ Принимает id рецепта, и увеличивает популярность продуктов, в составе на 1. """
     for prod in recipe.product.all():
         prod.used += 1
         prod.save()
